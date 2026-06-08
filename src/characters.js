11de4c67fee5991e61eb2277aa28716627ed7c92
Eigo-G <michael.english.be@gmail.comgit config --global user.name Eigo-Ggit config --global uswer.email michael.english.be@gmail.com>
2026-06-08 22:49:35 +0200

    Add character data and turn order system

---
+++ src/characters.js
@@ -0,0 +1,28 @@
+const playerTeam = [
+    { name: "Kael", hp: 1000, maxHp: 1000, attack: 150, speed: 80, element: "fire" },
+    { name: "Lyra", hp: 800, maxHp: 800, attack: 120, speed: 110, element: "wind" },
+    { name: "Gorath", hp: 1400, maxHp: 1400, attack: 180, speed: 50, element: "earth" }
+];
+
+const enemyTeam = [
+    { name: "Shade", hp: 900, maxHp: 900, attack: 140, speed: 90, element: "dark" },
+    { name: "Vex", hp: 750, maxHp: 750, attack: 130, speed: 120, element: "wind" },
+    { name: "Brute", hp: 1500, maxHp: 1500, attack: 170, speed: 40, element: "earth" }
+];
+
+console.log("Player Team:", playerTeam);
+console.log("Enemy Team:", enemyTeam);
+
+function getTurnOrder(playerTeam, enemyTeam) {
+    const allCharacters = [...playerTeam, ...enemyTeam];
+    
+    const turnOrder = allCharacters.sort((a, b) => b.speed - a.speed);
+    
+    return turnOrder;
+}
+
+const turnOrder = getTurnOrder(playerTeam, enemyTeam);
+
+turnOrder.forEach((character, index) => {
+    console.log(`Turn ${index + 1}: ${character.name} (Speed: ${character.speed})`);
+});
